1d6bcb13f7368dcddeb3e63a6abbabd29094eb91
Joao Paulo F. Salviano <jpfsalviano@gmail.com>
2019-03-12 17:48:22 -0300

    page 59

---
+++ chapter2/fiveTimes.py
@@ -0,0 +1,11 @@
+print('My name is')
+for i in range(5):
+    print('Jimmy Five Times (' + str(i) + ')')
+
+### an equivalent while loop:
+
+print('My name is')
+i = 0
+while i < 5:
+    print('Jimmy Five Times (' + str(i) + ')')
+    i += 1
