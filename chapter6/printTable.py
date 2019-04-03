560e554bc6a120123e378ca77f77dbc148331f57
Joao Paulo F. Salviano <jpfsalviano@gmail.com>
2019-04-03 17:40:36 -0300

    page 150

--- chapter6/printTable.py
+++ chapter6/printTable.py
@@ -5,27 +5,20 @@ tableData = [['apples', 'oranges', 'cherries', 'banana'],
              ['dogs', 'cats', 'moose', 'goose']]
 
 def printTable(listOfLists):
-    colWidths = [0] * len(tableData)
-    for listOfStrings in listOfLists:
+    colWidths = [0] * len(tableData)    # creates a list where the lengths of the longest strings will be stored
+    for index, listOfStrings in enumerate(listOfLists): # iterates in each list
         width = 0
-        i = 0
-        for string in listOfStrings:
+        for string in listOfStrings:    # checks the length of each string and store the longest
             if len(string) > width:
                 width = len(string)
-        colWidths[i] = width
-        i += 1
-    col = 0
+        colWidths[index] = width        # stores the longest length in the list previously created for that purpose
     line = 0
-    while col < len(listOfLists):
-        while line < len(listOfLists[col]):
-            print(listOfLists[col][line].rjust(colWidths[col]))
-            line += 1
-        col += 1
+    while line < len(listOfLists[0]):   # the number of items in a nested list is the number of lines in the table
+        col = 0
+        while col < len(listOfLists):   # the number of nested lists in the data list is the number of columns in the table
+            print(listOfLists[col][line].rjust(colWidths[col]), end=' ') # prints one item for each column
+            col += 1
+        line += 1
+        print('')                       # make sure next iteration will print in a newline
 
 printTable(tableData)
-    
-    # iterate in each list to evaluate the longest string
-    # set coldWidhts[i] = longest string length
-    # 2 while loops
-    # print tableData[i][0].rjust(colWidhts[i]) -> tableData[i+1][0].rjust(colWidhts[i+1])
-    # print tableData[i][1].rjust(colWidhts[i]) -> tableData[i+1][1].rjust(colWidths[i+1])
